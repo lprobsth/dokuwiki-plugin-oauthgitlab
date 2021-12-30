9c6c8d4b02cabe13ae63221bb30da58f8d9f21f1
Lukas Probsthain <lukas.probsthain@gmail.com>
2021-12-30 21:03:01 +0100

    add translation info for settings

---
+++ lang/de/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * english language file for oauthgitlab plugin
+ *
+ * @author Lukas Probsthain <lukas.probsthain@gmail.com>, 
+ */
+
+$lang['url']      = 'Die Basisadresse der gewünschten GitLab-Instanz. Beispiel: https://my.gilab.tld';
+$lang['key']      = 'Die Client ID der <a href="https://gitlab.com/-/profile/applications">GitLab Application</a>';
+$lang['secret']   = 'Das Client Secret der <a href="https://gitlab.com/-/profile/applications">GitLab Application</a>';
+$lang['rules']    = 'Optionale Regeln für die Benutzerdaten. Pro Zeile darf nur eine Regel eingetragen werden. <br>Beispiele:<br>user/can_create_project<br>user/external = 0<br>user/job_title != intern<br>groups/geeks<br>groups/comm = <br>projects/wargame';
---
+++ lang/en/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * english language file for oauthgitlab plugin
+ *
+ * @author Martin Panel, Lukas Probsthain <lukas.probsthain@gmail.com>, 
+ */
+
+$lang['url']      = 'The base URL of you Gitlab instance (if self-hosted). Example: https://my.gilab.tld';
+$lang['key']      = 'The Client ID of your registered <a href="https://gitlab.com/-/profile/applications">GitLab application</a>';
+$lang['secret']   = 'The Client Secret of your registered <a href="https://gitlab.com/-/profile/applications">Gitlab application</a>';
+$lang['rules']    = 'Optional new-line separated rules that must match the Gitlab user. Rules check if a key exist or a condition match user, groups, or projects data<br>Examples:<br>user/can_create_project<br>user/external = 0<br>user/job_title != intern<br>groups/geeks<br>groups/comm = <br>projects/wargame';
